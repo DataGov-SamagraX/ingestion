2538b7223cbd292db0c2b8188fa9d2bfc4dcd6fc
ChakshuGautam <chaks.gautam@gmail.com>
2022-04-29 10:31:09 +0530

    Test: Setup scaffold

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { join } from 'path';
+import { parse } from 'csv-parse';
+import fs = require('fs');
+import { from, Observable } from 'rxjs';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  jest.setTimeout(1000000);
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,10 +19,35 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/ (GET)', (done) => {
+    const parser = parse({ columns: true, trim: true });
+    const start = new Date().getTime();
+    const filePath = join(process.cwd(), 'test/test.data.csv');
+    const dataStream = fs.createReadStream(filePath, 'utf8');
+    const observable = from(dataStream.pipe(parser));
+    let totalPassengers = 0;
+    let totalTrips = 0;
+    observable.subscribe({
+      next(x) {
+        totalTrips++;
+        totalPassengers += parseInt(x.passenger_count);
+        // console.log(x);
+      },
+      error(err) {
+        console.error('something wrong occurred: ' + err);
+      },
+      complete() {
+        //end time
+        const end = new Date().getTime();
+        const time = end - start;
+        console.log(
+          `Total trips: ${totalTrips}`,
+          `Total passengers: ${totalPassengers}`,
+          `Total time: ${time}`,
+        );
+        console.log(`Stream speed: ${totalTrips / time} records/ms`);
+        done();
+      },
+    });
   });
 });
